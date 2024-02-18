6c290114de114b7993e9bbd792ac2183e5f7a5a9
satk0 <satko_a@op.pl>
2024-02-18 15:17:24 +0100

    Notice errors

--- usbcp.sh
+++ usbcp.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Colors for 'echo' command
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
 function main() {
 	fileFrom=$1
 	fileTo=$2
@@ -7,7 +12,13 @@ function main() {
 	startTime=$SECONDS
 
 	echo '1. Running rsync...'
-	rsync -avP "$fileFrom" "$fileTo"
+	rsyncErr=$(rsync -avP "$fileFrom" "$fileTo" 2>&1 > /dev/null)
+	if [ "$rsyncErr" != '' ]; then
+		echo -e "${RED}\"rsync\" execution error:${NC}"
+		echo "$rsyncErr"
+		echo -e "${RED}Quiting...${NC}"
+		return
+	fi
 
 	echo '2. Syncing...'
 
@@ -30,7 +41,7 @@ function main() {
 	elapsedTime=$(( SECONDS - startTime ))
 	echo 'Finished in:'
 	date -ud "@$elapsedTime" +'%H hr %M min %S sec'
-	#echo "Finished in: $elapsedTime seconds."
+	echo -e "${GREEN}Copied successfully!${NC}"
 }
 
 # https://stackoverflow.com/a/78015981
