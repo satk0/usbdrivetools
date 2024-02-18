20987e5ddfbe242929a70271c1bcd76159508935
satk0 <satko_a@op.pl>
2024-02-18 13:56:49 +0100

    Add time and make syncing stage don't take too much terminal space

--- usbcp.sh
+++ usbcp.sh
@@ -3,6 +3,8 @@
 fileFrom=$1
 fileTo=$2
 
+start_time=$SECONDS
+
 echo '1. Running rsync...'
 rsync -avP "$fileFrom" "$fileTo"
 
@@ -13,7 +15,15 @@ res=""
 while [ "$res" = "" ]; do
 	res=$(kill -0 "$pID" 2>&1 > /dev/null)
 	tmp=$(grep -E '(Dirty|Writeback):' /proc/meminfo)
-	echo "$tmp"
 
-	sleep 1
+	echo "$tmp"
+	sleep 0.5
+	
+	tput cuu 2
+	tput ed
 done
+
+elapsed_time=$(( SECONDS - start_time ))
+#
+echo "$tmp"
+echo "Done in: $elapsed_time seconds."
