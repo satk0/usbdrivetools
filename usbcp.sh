5efc47b221acb036433d62582c662a5af7a64048
satk0 <satko_a@op.pl>
2024-02-18 14:51:54 +0100

    Make it more general

--- usbcp.sh
+++ usbcp.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/env bash
 
-fileFrom=$1
-fileTo=$2
+function main() {
+	fileFrom=$1
+	fileTo=$2
 
-start_time=$SECONDS
+	start_time=$SECONDS
 
-echo '1. Running rsync...'
-rsync -avP "$fileFrom" "$fileTo"
+	echo '1. Running rsync...'
+	rsync -avP "$fileFrom" "$fileTo"
 
-echo '2. Syncing...'
-sync &
-pID=$!; 
-res=""
-while [ "$res" = "" ]; do
-	res=$(kill -0 "$pID" 2>&1 > /dev/null)
-	tmp=$(grep -E '(Dirty|Writeback):' /proc/meminfo)
+	echo '2. Syncing...'
+
+	sync &
+	pID=$!; 
+	res=""
+
+	while [ "$res" = "" ]; do
+		res=$(kill -0 "$pID" 2>&1 > /dev/null)
+		tmp=$(grep -E '(Dirty|Writeback):' /proc/meminfo)
+
+		echo "$tmp"
+		sleep 0.5
+		
+		clearLastLines 2
+	done
+
+	elapsed_time=$(( SECONDS - start_time ))
 
 	echo "$tmp"
-	sleep 0.5
-	
-	tput cuu 2
-	tput ed
-done
+	echo "Finished in: $elapsed_time seconds."
+}
 
-elapsed_time=$(( SECONDS - start_time ))
-#
-echo "$tmp"
-echo "Done in: $elapsed_time seconds."
+# https://stackoverflow.com/a/78015981
+function clearLastLines() {
+	local linesToClear=$1
+
+	for (( i=0; i<linesToClear; i++ )); do
+		tput cuu 1
+		tput el
+	done
+}
+
+main "$@"; exit
